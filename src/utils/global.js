57d8320674b36d67c32e2897521bf8ef29d6834f
bernardonascimento <bernardonasciimento@gmail.com>
2020-08-23 12:42:51 -0300

    MODIFIED function formatDate in global

--- src/utils/global.js
+++ src/utils/global.js
@@ -2,9 +2,10 @@ const formatDateToLocaleString = (str) => {
 	if (!str) return "";
 	let data = new Date(str);
 
-	return `${data.getUTCDate()}/${("00" + (data.getUTCMonth() + 1)).slice(
-		-2
-	)}/${data.getUTCFullYear()}`;
+	return `${("00" + data.getUTCDate()).slice(-2)}/${(
+		"00" +
+		(data.getUTCMonth() + 1)
+	).slice(-2)}/${data.getUTCFullYear()}`;
 };
 
 export { formatDateToLocaleString };
